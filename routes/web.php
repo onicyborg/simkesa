180195dd7406c3b7e038de0787f766b9f8f56d37
Ace <akhmadfauzy40@gmail.com>
2025-11-29 09:35:03 +0700

    settings: multi role

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+Route::group(['middleware' => 'role:admin'], function () {
+
+});
+
+Route::group(['middleware' => 'role:guru'], function () {
+
+});
+
+Route::group(['middleware' => 'role:siswa'], function () {
+
 });
